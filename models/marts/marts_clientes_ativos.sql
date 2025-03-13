dd09927ff089d6d0f9e6e284dd8af55559407364
Lucas Alves <lucasalves_taz@hotmail.com>
2025-03-13 00:38:20 -0300

    up f marts

--- models/marts/marts_clientes_ativos.sql
+++ models/marts/marts_clientes_ativos.sql
@@ -1,6 +1,10 @@
-SELECT
-    cliente_id,
-    COUNT(id) AS total_pedidos
-FROM {{ ref('stg_pedidos') }}
-WHERE data_pedido >= CURRENT_DATE - INTERVAL '2 months'
-GROUP BY cliente_id DESC
+WITH clientes_ativos AS (
+    SELECT
+        cliente_id,
+        COUNT(id) AS total_pedidos
+    FROM {{ ref('stg_pedidos') }}
+    WHERE data_pedido >= CURRENT_DATE - INTERVAL '2 months'
+    GROUP BY cliente_id
+    ORDER BY total_pedidos DESC
+)
+SELECT * FROM clientes_ativos
