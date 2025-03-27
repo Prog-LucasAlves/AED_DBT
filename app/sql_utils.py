50c9eb32ec667d18ad4704a634dcb39be41b060b
Lucas Alves <lucasalves_taz@hotmail.com>
2025-03-26 22:40:15 -0300

    up sql_utils.py

--- app/sql_utils.py
+++ app/sql_utils.py
@@ -9,3 +9,12 @@ def execute_query(query):
             return result.fetchall()
     except Exception as e:
         raise f"❌ Erro ao executar a consulta: {str(e)}"
+
+
+def test_connection():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return "✅ Conexão com o banco de dados bem-sucedida."
+    except Exception as e:
+        raise f"❌ Erro ao conectar ao banco de dados: {str(e)}"
