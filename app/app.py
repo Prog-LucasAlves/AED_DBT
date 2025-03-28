9c725edef6cfd54460c5d6ade95385b27c016730
Lucas Alves <lucasalves_taz@hotmail.com>
2025-03-28 14:17:12 -0300

    up app.py

--- app/app.py
+++ app/app.py
@@ -8,7 +8,8 @@ st.set_page_config(page_title="Dashboard de Vendas", layout="wide")
 st.markdown("<h1 style='text-align: center;'>📈 Dashboard de Vendas</h1>", unsafe_allow_html=True)
 
 ####### Query ######
-df = pd.DataFrame(sql_utils.execute_query("SELECT * FROM public_raw.raw_valor_total_canal_venda"))
+valorTotalCanalVenda = pd.DataFrame(sql_utils.execute_query("SELECT * FROM public_raw.raw_valor_total_canal_venda"))
+valorTotalFormaPagamento = pd.DataFrame(sql_utils.execute_query("SELECT * FROM public_raw.raw_valor_total_forma_pagamento"))
 
 ####### Colunas #####
 col1, col2, col3 = st.columns(3)
@@ -18,10 +19,10 @@ with col1:
 
     # Exibindo os dados em formato de tabela
     st.markdown("<h2 style='font-size:22px;'>📊 Dados de Vendas por Canal de Venda</h2>", unsafe_allow_html=True)
-    st.dataframe(df)
+    st.dataframe(valorTotalCanalVenda)
 
     # Criando um gráfico de barras para mostrar o total de vendas por canal
-    fig = px.bar(df,
+    fig = px.bar(valorTotalCanalVenda,
                  x='descricao_canal_venda',
                  y='total_formatado',
                  title="Total de Vendas por Canal de Venda",
@@ -34,12 +35,47 @@ with col1:
     st.plotly_chart(fig)
 
     # Verifica se a coluna 'percentual' existe antes de criar o gráfico de pizza
-    if 'percentual' in df.columns:
-        fig_pie = px.pie(df,
+    if 'percentual' in valorTotalCanalVenda.columns:
+        fig_pie = px.pie(valorTotalCanalVenda,
                          names='descricao_canal_venda',
                          values='percentual',
                          title="Distribuição Percentual de Vendas por Canal de Venda",
                          labels={'descricao_canal_venda': 'Canal de Venda', 'percentual': 'Percentual'})
+        fig_pie.update_layout(title_font_size=22)
+
+        # Exibindo o gráfico de pizza dentro do mesmo bloco
+        st.plotly_chart(fig_pie)
+    else:
+        st.warning("A coluna 'percentual' não foi encontrada no banco de dados.")
+
+####### 🔵 Vendas por Forma de Pagamento 🔵 #######
+
+with col2:
+
+    # Exibindo os dados em formato de tabela
+    st.markdown("<h2 style='font-size:22px;'>📊 Dados de Vendas por Canal de Venda</h2>", unsafe_allow_html=True)
+    st.dataframe(valorTotalFormaPagamento)
+
+    # Criando um gráfico de barras para mostrar o total de vendas por canal
+    fig = px.bar(valorTotalFormaPagamento,
+                 x='descricao_metodo_pagameto',
+                 y='total_formatado',
+                 title="Total de Vendas por Canal de Venda",
+                 labels={'descricao_metodo_pagameto': 'Canal de Venda', 'total_formatado': 'Total de Vendas'},
+                 color='total_formatado',
+                 color_continuous_scale='Viridis')
+    fig.update_layout(title_font_size=22)
+
+    # Exibindo o gráfico
+    st.plotly_chart(fig)
+
+    # Verifica se a coluna 'percentual' existe antes de criar o gráfico de pizza
+    if 'percentual' in valorTotalFormaPagamento.columns:
+        fig_pie = px.pie(valorTotalFormaPagamento,
+                         names='descricao_metodo_pagameto',
+                         values='percentual',
+                         title="Distribuição Percentual de Vendas por Canal de Venda",
+                         labels={'descricao_metodo_pagameto': 'Canal de Venda', 'percentual': 'Percentual'})
         fig_pie.update_layout(title_font_size=22)
 
         # Exibindo o gráfico de pizza dentro do mesmo bloco
